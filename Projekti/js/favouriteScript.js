74e288d6b1e780b262813553a0ae32c46d2a2353
Jere Salmensaari <jere.salmensaari@gmail.com>
2020-05-07 12:41:21 +0300

    Moved favourites into their correct folder

--- Projekti/js/favouriteScript.js
+++ Projekti/js/favouriteScript.js
@@ -1,17 +1,27 @@
+const clearButton = document.getElementById("clearButton");
 const ul = document.getElementById("favouriteList");
-var favourites = JSON.parse(localStorage.getItem("favourites"));
-favourites = favourites.sort();
-const suosikit = document.querySelector("h1");
-console.log("Suosikit ID-lista: " + favourites);
+var favouriteList = JSON.parse(localStorage.getItem("favourites"));
+favouriteList = favouriteList.sort();
 
-for(var i = 0; i < favourites.length; i++){
-    apiurl_suosikki = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + favourites[i];
+//Removes non-unique entries from array
+favouriteList = favouriteList.filter(onlyUnique);
+
+const suosikit = document.querySelector("h1");
+console.log("Suosikit ID-lista: " + favouriteList);
+clearButton.addEventListener('click', clearList);
+
+for(var i = 0; i < favouriteList.length; i++){
+    apiurl_suosikki = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + favouriteList[i];
     fetch(apiurl_suosikki).then(function(response) {
         return response.json();
     }).then(function(json) {
         naytaVastaus(json);
     })
 }
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
 function naytaVastaus(jsonData) {
     var otsikko = document.getElementById("otsikko");
     console.log(jsonData)
@@ -41,4 +51,12 @@ function naytaVastaus(jsonData) {
 function getDrink(drinkID) {
      localStorage.setItem("drinkID", drinkID);
      window.location.href = "./results.html";
+}
+function clearList(jsonData) {
+    if(confirm("Are you sure? Press OK to clear favourites.")){
+        favouriteList = [];
+        localStorage.setItem("favourites", JSON.stringify(favouriteList));
+        alert("Favourites cleared!");
+        location.reload();
+    }
 }
